9c365c95d7d20819f2eb5772dea4cbdb4f5bdc9c
Zachary-Kiz <zachary.kizell@gmail.com>
2026-04-19 12:39:16 -0400

    add function to retrieve book from postgres

---
+++ postgres_funcs.py
@@ -0,0 +1,30 @@
+import os
+import psycopg2
+
+from dotenv import load_dotenv
+from psycopg2.extras import RealDictCursor
+
+
+load_dotenv()
+
+POSTGRESQL_DATABASE = os.getenv('POSTGRESQL_DATABASE')
+POSTGRESQL_USER = os.getenv('POSTGRESQL_USER')
+POSTGRESQL_PWD = os.getenv('POSTGRESQL_PWD')
+
+connection = psycopg2.connect(database=POSTGRESQL_DATABASE, user=POSTGRESQL_USER, password=POSTGRESQL_PWD, port=5432, cursor_factory=RealDictCursor)
+
+def getBook():
+    cursor = connection.cursor()
+    cursor.execute("""
+                   SELECT * FROM bookData 
+                   WHERE downloaded = FALSE
+                   ORDER BY RANDOM()
+                   LIMIT 1
+                   """)
+
+    # Fetch all rows from database
+    record = cursor.fetchone()
+
+    return record
+
+getBook()
